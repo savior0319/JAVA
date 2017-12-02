9bc1df3d789ffa8fd95d4016f22993d3de16eabb
AHJ <you@example.com>
2017-12-03 02:56:20 +0900

    -

---
+++ JAVA/etc/_TryCatch/Tc.java
@@ -0,0 +1,14 @@
+package _TryCatch;
+
+public class Tc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.print("Hello World");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
