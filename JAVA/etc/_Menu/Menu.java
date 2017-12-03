c09609c46f1f2efa3a1745bd8af3fab3d378a6ba
AHJ <you@example.com>
2017-12-03 20:51:08 +0900

    -

--- JAVA/etc/_Menu/Menu.java
+++ JAVA/etc/_Menu/Menu.java
@@ -13,9 +13,9 @@ public class Menu extends JFrame implements ActionListener {
 
 	private Container ct = getContentPane();
 	private JLabel jl = new JLabel("", JLabel.CENTER);
-	private JMenuBar jmb = new JMenuBar();
-	private JMenuItem item1, item2;
-	private JMenu m1 = new JMenu("메뉴1 (A)");
+	private JMenuBar jmb = new JMenuBar(); // 메뉴바 추가
+	private JMenu m1 = new JMenu("메뉴1 (A)"); // 메뉴 목록 추가
+	private JMenuItem item1, item2; // m1(메뉴1) 목록의 아이템 추가
 
 	public Menu() {
 		setTitle("메뉴바");
@@ -28,22 +28,21 @@ public class Menu extends JFrame implements ActionListener {
 		jl.setFont(new Font("맑은고딕", Font.BOLD, 20));
 		ct.add(jl, BorderLayout.CENTER);
 
-		jmb.add(m1);
+		jmb.add(m1); // 메뉴바에 m1 메뉴 삽입
 		m1.setMnemonic(KeyEvent.VK_A); // 단축키 설정 ALT + A
-		item1 = new JMenuItem("확 인");
-		item2 = new JMenuItem("종 료");
+		item1 = new JMenuItem("확 인"); 
 		item1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, 
 				ActionEvent.CTRL_MASK)); // item1 단축키 설정
-		m1.add(item1);
+		m1.add(item1); // m1 에 아이템1 삽입
 		m1.addSeparator();
+		item2 = new JMenuItem("종 료");
 		item2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, 
 				ActionEvent.CTRL_MASK)); // item2 단축키 설정
-		m1.add(item2);
-		setJMenuBar(jmb);
+		m1.add(item2); // m1 에 아이템2 삽입
+		setJMenuBar(jmb); // 메뉴바를 프레임에 추가
 
 		item1.addActionListener(this);
 		item2.addActionListener(this);
-
 	}
 
 	public static void main(String[] args) {
@@ -57,7 +56,7 @@ public class Menu extends JFrame implements ActionListener {
 			jl.setText("확인");
 		}
 		else if (arg0.getActionCommand() == "종 료") {
-			System.exit(0);
+			System.exit(0); // 프로그램 종료
 		}
 	}
 }
