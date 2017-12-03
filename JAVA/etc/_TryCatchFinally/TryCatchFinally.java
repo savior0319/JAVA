5ae033f3e5e6cef6af39ddf762bf50ce0ec3912b
AHJ <you@example.com>
2017-12-03 21:18:38 +0900

    -

---
+++ JAVA/etc/_TryCatchFinally/TryCatchFinally.java
@@ -0,0 +1,25 @@
+package _TryCatchFinally;
+
+public class TryCatchFinally {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("Hello World");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String[] str = new String[1];
+		str[0] = "Hello World";
+		
+		try {
+			System.out.println(str[1]);
+		} catch (Exception e) {
+			System.out.println("배열 오류 발생");
+		} finally {
+			System.out.println(str[0]);
+		}
+	}
+
+}
