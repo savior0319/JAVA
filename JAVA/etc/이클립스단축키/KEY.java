1cbd650ee00f78a5754b7ed8c7b9308ade20991b
AHJ <you@example.com>
2017-12-03 21:33:40 +0900

    -

---
+++ JAVA/etc/이클립스단축키/KEY.java
@@ -0,0 +1,69 @@
+package 이클립스단축키;
+/*
+ * 1. Ctrl + Shift + L : 이클립스 전체 단축키를 볼 수 있다.
+ * 
+ * 2. Ctrl + F11 : 최근 실행한 Run 실행
+ * 
+ * 3. Ctrl + D : 줄 삭제
+ * 
+ * 4. Alt + ↑ , Alt + ↓ : 줄 위치 바꿈
+ * 
+ * 5. Ctrl + Shift + O : 사용하지 않는 import 삭제, 필요한 import 추가
+ * 
+ * 6. Ctrl + H : 검색 창을 open
+ * 
+ * 7. Ctrl + F : 찾기/바꾸기 창을 open
+ * 
+ * 8. Ctrl + L : 입력한 라인의 줄로 이동
+ * 
+ * 9. Ctrl + Page Up , Ctrl + Page Down : 코드 편집 창에 여러 개의 창이 띄워져 있을 경우 좌/우 창 이동
+ * 
+ * 10. Ctrl + F6 : 현재 띄워져 있는 파일간 이동
+ * 
+ * 11. Ctrl + F7 : 뷰간 이동 ( 네비게이션 )
+ * 
+ * 12. Ctrl + W : 에디터 창 닫기
+ * 
+ * 13. Ctrl + Shift + W : 열려있는 모든 에디터 창 닫기
+ * 
+ * 14. Alt + Shift + A : 에디터모드, 다량의 데이터 복사 붗여넣기 가능, 드래그하여 영역 지정하여 수정 가능, 다수 커서이용
+ * 가능
+ * 
+ * 15. Ctrl + / : 한줄 주석처리 토글 - JAVA, javascript, properties 파일 등에서 가능 - HTML,
+ * XML 파일에서 불가
+ * 
+ * 16. Ctrl + Shift + / : 블록 주석처리 토글
+ * 
+ * 17. Ctrl + Shift + F, Ctrl + I : 코드 포멧팅 ( 줄 맞춤 )
+ * 
+ * 18. Ctrl + Shift + Delete : 현재 위치에서 라인 끝까지 삭제
+ * 
+ * 19. Ctrl + Alt + ↓ : 한줄 복사 후 붙여넣기 ( 커서가 위치한 라인의 문장 정체를 복사하여 바로 아랫줄에 붙여넣음 )
+ * 
+ * 20. Ctrl + Shift + Space : 메소드의 가로 안에 커서를 놓고 이 키를 누르면 파라메터 타입 힌트
+ * 
+ * 21. Ctrl + K : 블럭으로 지정된 문자열을 찾는다. ( 같은 문서 내 )
+ * 
+ * 22. Ctrl + Shift + K : 역으로 찾고자 하는 문자열을 찾아감
+ * 
+ * 23. Ctrl + Shift + G : 해당 메서드, 객체, 변수가 사용(호출) 된 모든 곳을 찾는다.
+ * 
+ * 24. Ctrl + Alt + G : 해당 문자열이 사용된 모든 곳을 찾는다. ( workspace 내 모든 파일 )
+ * 
+ * 24. F3 : 선언 위치로 이동. 변수의 경우에는 변수가 선언된 위치로, 메소드의 경우에는 메소드의 본문이 있는 곳으로 이동
+ * 
+ * 25. Ctrl + SpaceBar : 자동완성 - sysout : System.out.println() - try : try ~
+ * catch문 - for : for문 - do : do~while문 - while : while문
+ * 
+ * 26. Alt + Shift + J : 파일/메소드 자동 주석 생성
+ * 
+ * 27. Ctrl + Q : 마지막으료 편집한 곳으로 이동
+ * 
+ * 28. Ctrl + E : 다른 편집창으로 이동
+ * 
+ * 29. Ctrl + M : 현재 뷰/편집창을 최대로 키움
+ * 
+ * 30. Ctrl + Shift + C : 해당 line을 주석처리 ( HTML, XML 모두 가능 )
+ * 
+ * 31. Ctrl + Shift + R : 파일 찾기
+ */
