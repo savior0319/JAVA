9c31872c4633704d7014bfa92a793aa2f5a0d61e
savio <savior0319@naver.com>
2017-12-04 20:30:10 +0900

    -

--- JAVA/etc/MemoPad_Menubar/MemoPad.java
+++ JAVA/etc/MemoPad_Menubar/MemoPad.java
@@ -18,7 +18,7 @@ public class MemoPad extends JFrame implements ActionListener {
 			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 	private JMenuBar jmb = new JMenuBar(); // 메뉴바 추가
 	private JMenu m1 = new JMenu("메뉴 (A)"); // 메뉴 목록 추가
-	private JMenuItem item1, item2, item3; // m1(메뉴) 목록의 아이템 추가
+	private JMenuItem item0, item1, item2, item3; // m1(메뉴) 목록의 아이템 추가
 	private JLabel jl1 = new JLabel("메모장");
 	private File fn;
 	private FileReader fr;
@@ -45,6 +45,10 @@ public class MemoPad extends JFrame implements ActionListener {
 
 		jmb.add(m1); // 메뉴바에 m1 메뉴 삽입
 		m1.setMnemonic(KeyEvent.VK_A); // 단축키 설정 ALT + A
+		item0 = new JMenuItem("새 문서");
+		item0.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		m1.add(item0);
+		m1.addSeparator();
 		item1 = new JMenuItem("열 기");
 		item1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK)); // item1 단축키 설정
 		m1.add(item1); // m1 에 아이템1 삽입
@@ -58,6 +62,7 @@ public class MemoPad extends JFrame implements ActionListener {
 		m1.add(item3); // m1 에 아이템2 삽입
 		setJMenuBar(jmb); // 메뉴바를 프레임에 추가
 
+		item0.addActionListener(this);
 		item1.addActionListener(this);
 		item2.addActionListener(this);
 		item3.addActionListener(this);
@@ -71,7 +76,39 @@ public class MemoPad extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		String check = jta.getText();
 		int nostring = 0;
-		if (arg0.getActionCommand() == "열 기") {
+		if (arg0.getActionCommand() == "새 문서") {
+			if (nostring != check.length()) { // 내용이 있을 경우 열기전에 저장
+				jl1.setText("내용이 있음. (새로 만들기 전 저장)");
+				if (jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+					fn = jfc.getSelectedFile();
+					try {
+						fw = new FileWriter(fn + ".txt");
+					} catch (IOException e) {
+						jl1.setText("쓰기에러.");
+					}
+					String str = jta.getText();
+					bw = new BufferedWriter(fw);
+					try {
+						for (int i = 0; i < str.length(); i++) { // 띄어쓰기시 줄 바꿈
+							if (str.charAt(i) == '\n') {
+								bw.newLine();
+							} else
+								bw.write(str.charAt(i));
+						}
+						bw.close();
+						jl1.setText("파일저장 완료.");
+					} catch (IOException e) {
+						jl1.setText("쓰기에러.");
+					}
+				}
+				jta.setText(null);
+			} else if (nostring == check.length()) {
+				jl1.setText("새 문서");
+				jta.setText(null);
+			}
+		}
+
+		else if (arg0.getActionCommand() == "열 기") {
 
 			if (nostring != check.length()) { // 내용이 있을 경우 열기전에 저장
 				jl1.setText("내용이 있음. (새로 열기 전 저장)");
@@ -97,8 +134,9 @@ public class MemoPad extends JFrame implements ActionListener {
 						jl1.setText("쓰기에러.");
 					}
 				}
-			} else if (nostring == check.length()) { // 내용이 없을 경우 열기
 
+			} else if (nostring == check.length()) { // 내용이 없을 경우 열기
+				jl1.setText("열기");
 				if (jfc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) { // 열기 chooser 생성
 					fn = jfc.getSelectedFile();
 					try {
@@ -122,7 +160,7 @@ public class MemoPad extends JFrame implements ActionListener {
 
 			}
 		} else if (arg0.getActionCommand() == "저 장") {
-
+			jl1.setText("저장");
 			if (jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
 				fn = jfc.getSelectedFile();
 				try {
