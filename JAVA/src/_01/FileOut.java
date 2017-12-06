81ee62ab71df4c3de981c9228c87f4228aadfa23
savio <savior0319@naver.com>
2017-12-06 12:20:40 +0900

    -

---
+++ JAVA/src/_01/FileOut.java
@@ -0,0 +1,35 @@
+package _01;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class FileOut {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String text;
+
+		System.out.println("출력할 문장 입력");
+		System.out.print("->");
+		text = sc.nextLine();
+		sc.close();
+		writeText(text);
+	}
+
+	private static void writeText(String text) {
+
+		try {
+			FileWriter fw = new FileWriter("test.txt");
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(text);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("입력한 문장 저장 완료");
+		System.out.println("프로그램 종료");
+	}
+}
