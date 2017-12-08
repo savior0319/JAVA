af7df41ac0d2f9cec3eb8ed77828bdbbb9b80cd9
savio <savior0319@naver.com>
2017-12-09 01:53:14 +0900

    -

--- JAVA/기말.cpp
+++ JAVA/기말.cpp
@@ -51,6 +51,8 @@ void save(struct info *data, int count)
 int main() {
 	system("tItlE 성적관리");
 	system("CoLOr f0");
+	system("mode con:cols=96 lines=20");
+
 	{
 		struct info data[MAX];
 		int count = 0;
@@ -63,42 +65,42 @@ int main() {
 		load(data, &count);
 		while (start)
 		{
-			printf("---메뉴---\n");
-			printf("1.입력\n");
-			printf("2.저장\n");
-			printf("3.출력\n");
-			printf("4.삭제\n");
-			printf("5.갱신\n");
-			printf("6.종료\n");
-			printf("-> ");
+			printf("\t\t\t\t\t---메뉴---\n");
+			printf("\t\t\t\t\t1.입력\n");
+			printf("\t\t\t\t\t2.저장\n");
+			printf("\t\t\t\t\t3.출력\n");
+			printf("\t\t\t\t\t4.삭제\n");
+			printf("\t\t\t\t\t5.갱신\n");
+			printf("\t\t\t\t\t6.종료\n");
+			printf("\t\t\t\t\t-> ");
 			scanf("%d", &choice);
 		
 			if (choice == 1) // 입력
 			{
 				system("cls");
-				printf("---정보입력---\n");
-				printf("이름 :");
+				printf("\t\t\t\t\t---정보입력---\n");
+				printf("\t\t\t\t\t이름 :");
 				scanf("%s", data[count].name);
-				printf("전화번호 :");
+				printf("\t\t\t\t\t전화번호 :");
 				scanf("%s", data[count].phone);
-				printf("C언어 점수:");
+				printf("\t\t\t\t\tC언어 점수:");
 				scanf("%lf", &data[count].cp);
-				printf("정보보안 점수:");
+				printf("\t\t\t\t\t정보보안 점수:");
 				scanf("%lf", &data[count].is);
-				printf("네트워크 점수:");
+				printf("\t\t\t\t\t네트워크 점수:");
 				scanf("%lf", &data[count].net);
 				data[count].avg = (data[count].cp + data[count].is + data[count].net)/3;
 				count++;
 
-				printf("정보가 입력되었습니다.");
+				printf("\t\t\t\t\t정보가 입력되었습니다.");
 			}
 			else if (choice == 2) { // 저장
 				save(data, count);
-				printf("정보가 저장되었습니다.");
+				printf("\t\t\t\t\t정보가 저장되었습니다.");
 			}
 			else if (choice == 3) { // 출력
 				system("cls");
-				printf("---출력---\n");		
+				printf("\t\t\t\t\t---출력---\n");		
 				printf("------------------------------------------------------------------------------------------------\n");
 				printf("이 름\t 전화번호\t C 언 어\t 정보보안\t 네트워크\t 평균\t\t 등급\n");
 				printf("------------------------------------------------------------------------------------------------\n");
@@ -125,14 +127,23 @@ int main() {
 							data[i].name, data[i].phone, data[i].cp, data[i].is, data[i].net, data[i].avg);
 					}
 				}
-				printf("\n출력이 완료 되었습니다.\n");
+				printf("\n\t\t\t\t\t출력이 완료 되었습니다.\n");
 			}
 			else if (choice == 4) { // 삭제
 				system("cls");
-				printf("---삭제---\n");
-				printf("삭제할 이름 입력\n");
-				printf("-> ");
+				printf("\t\t\t\t\t---삭제---\n");
+				printf("--------------------------------------------------------------------------------------\n");
+				printf("이 름\t 전화번호\t C 언 어\t 정보보안\t 네트워크\t 평균\t\t\n");
+				printf("--------------------------------------------------------------------------------------\n");
+					for (i = 0; i <count; i++) {
+						printf("%s\t %s\t %.0f\t\t %.0f\t\t %.0f\t\t %.2f\t\t\n",
+							data[i].name, data[i].phone, data[i].cp, data[i].is, data[i].net, data[i].avg);
+						}
+				printf("\t\t\t\t\t삭제할 이름 입력\n");
+				printf("\t\t\t\t\t-> ");
 				scanf("%s", find);
+		
+				
 				for (i = 0; i < count; i++)
 				{
 					if (strcmp(data[i].name, find) == 0)
@@ -142,14 +153,14 @@ int main() {
 						count--;
 					}
 				}
-				printf("\n삭제되었습니다.\n");
+				printf("\n\t\t\t\t\t삭제되었습니다.\n");
 			}
 
 			else if (choice == 5) { // 갱신
 				system("cls");
-				printf("---갱신---\n");
-				printf("갱신할 이름 입력\n");
-				printf("-> ");
+				printf("\t\t\t\t\t---갱신---\n");
+				printf("\t\t\t\t\t갱신할 이름 입력\n");
+				printf("\t\t\t\t\t-> ");
 				scanf("%s", find);
 				printf("--------------------------------------------------------------------------------------\n");
 				printf("이 름\t 전화번호\t C 언 어\t 정보보안\t 네트워크\t 평균\t\t\n");
@@ -160,26 +171,26 @@ int main() {
 						printf("%s\t %s\t %.0f\t\t %.0f\t\t %.0f\t\t %.2f\t\t\n",
 							data[i].name, data[i].phone, data[i].cp, data[i].is, data[i].net, data[i].avg);
 						strcpy(data[i].name, find);
-						printf("\n변경 할 전화번호 : ");
+						printf("\n\t\t\t\t\t변경 할 전화번호 : ");
 						scanf("%s", cphone);
 						strcpy(data[i].phone, cphone);
-						printf("변경 할 C언어 점수 :");
+						printf("\t\t\t\t\t변경 할 C언어 점수 :");
 						scanf("%lf", &ccp);
 						data[i].cp = ccp;
-						printf("변경 할 정보보안 점수 :");
+						printf("\t\t\t\t\t변경 할 정보보안 점수 :");
 						scanf("%lf", &cis);
 						data[i].is = cis;
-						printf("변경 할 네트워크 점수 :");
+						printf("\t\t\t\t\t변경 할 네트워크 점수 :");
 						scanf("%lf", &cnet);
 						data[i].net = cnet;
 						data[i].avg = (data[i].cp + data[i].is + data[i].net) / 3;
 					}
 				}
-				printf("\n갱신 되었습니다.\n");
+				printf("\n\t\t\t\t\t갱신 되었습니다.\n");
 			}
 			else if(choice == 6){ // 종료
 				start = false;
-				printf("\n프로그램 종료\n");
+				printf("\n\t\t\t\t\t프로그램 종료\n");
 			}
 			_getch();
 			system("cls");
@@ -187,4 +198,3 @@ int main() {
 		return 0;
 	}
 }
-
