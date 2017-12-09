c5ab425b43708720ad2ac59a7fb80f5d79c3fa6d
savio <savior0319@naver.com>
2017-12-09 22:01:42 +0900

    -

--- JAVA/etc/_Oracle/Test.java
+++ JAVA/etc/_Oracle/Test.java
@@ -1,21 +1,31 @@
 package _Oracle;
 
 import java.sql.*; 
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
         String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-        String DB_USER = "enter ID";
-        String DB_PASSWORD = "enter PASSWORD";
+        String DB_USER = "ID";
+        String DB_PASSWORD = "PW";
 
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-
+        String sql = null;
+        
+        System.out.println("SQL 문을 입력하세요 (alter / insert / delete) : ");
+        sql = sc.nextLine();
+        sc.close();
         String query = "SELECT * FROM test";
+
         try {
             // 드라이버를 로딩한다.
             Class.forName("oracle.jdbc.driver.OracleDriver");
+            System.out.print("JDBC 연결 완료\n");
         } catch (ClassNotFoundException e ) {
            System.out.println("드라이버 로드 실패");
         }
@@ -26,6 +36,9 @@ public class Test {
             // Statement를 가져온다.
             stmt = conn.createStatement();
             // SQL문을 실행한다.
+
+            stmt.executeUpdate(sql);
+            
             rs = stmt.executeQuery(query);
 
             while (rs.next()) { 
